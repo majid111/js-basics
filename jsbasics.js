c57f7f02eadbeb270363d72b9d6c91953a2fdc39
majid111 <abdullahalmajid@gmail.com>
2024-01-30 03:15:03 +0600

    first files setting

---
+++ jsbasics.js
@@ -0,0 +1,15 @@
+let num,i,a,b,c;
+
+num =500;
+if(num>=100){
+    i = "Above 100";
+}
+else if(num>=50){
+    i= "between 50 and 100";
+}
+else{
+    i="less than 50";
+}
+
+console.log(i)
+console.log("hello World")
