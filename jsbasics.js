23f5794bb79299823a47d6d0db9fcbe548faa71a
majid111 <abdullahalmajid@gmail.com>
2024-02-01 01:59:03 +0600

    array

--- jsbasics.js
+++ jsbasics.js
@@ -1,15 +1,34 @@
-let num,i,a,b,c;
+let num, i, a, b, c;
 
-num =500;
-if(num>=100){
+num = 500;
+if (num >= 100) {
     i = "Above 100";
 }
-else if(num>=50){
-    i= "between 50 and 100";
+else if (num >= 50) {
+    i = "between 50 and 100";
 }
-else{
-    i="less than 50";
+else {
+    i = "less than 50";
 }
 
-console.log(i)
-console.log("hello World")
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(a.length)
+
+// a.push =1;
+a.pop();
+// a.push(10, 1, 2, 3);
+// a.shift();
+// a.unshift (1, 1, 1);
+c=a.includes(2,3);
+b = a.indexOf(5);
+
+
+
+
+console.log(c)
+console.log(a.length)
+console.log(a)
+console.log(b)
+// console.log(a.findIndex)
+// console.log(a.lastIndexOf(5))
+// console.log("hello World")
