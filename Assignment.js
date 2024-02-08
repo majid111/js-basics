e94798110c95f6f06b17b1c768aad5ee5ba85be0
majid111 <abdullahalmajid@gmail.com>
2024-02-09 04:28:34 +0600

    assignment done

--- Assignment.js
+++ Assignment.js
@@ -34,48 +34,119 @@ function calculateMoney(ticketSale) {
 
 function checkName(name) {
     if (typeof name === "string") {
-       const name2=name.toLowerCase();
-       const lastWord=name2.length-1;
-       if(name2[lastWord]==='a' || name2[lastWord]==='e' || name2[lastWord]==='i' || name2[lastWord]==='o' || name2[lastWord]==='u' || name2[lastWord]==='w' || name2[lastWord]==='y'){
+        const name2 = name.toLowerCase();
+        const lastWord = name2.length - 1;
+        if (name2[lastWord] === 'a' || name2[lastWord] === 'e' || name2[lastWord] === 'i' || name2[lastWord] === 'o' || name2[lastWord] === 'u' || name2[lastWord] === 'w' || name2[lastWord] === 'y') {
 
-           return "Good Name";
-       }
-       else{
-        return "Bad Name";
-       }
-        
+            return "Good Name";
+        }
+        else {
+            return "Bad Name";
+        }
+
     }
     else {
         return "Invalid";
     }
-    }
+}
 
-    /////////////////////////////////////
+/////////////////////////////////////
 
-    function deleteInvalids(array) {
-        if (Array.isArray(array) === true) {
-            const outputArray=[ ];
-            for(i=0;i<array.length;i++){
-                if(!isNaN(array[i]) && typeof array[i]==='number'){
-                    outputArray.push(array[i]);
-                }
-                
+function deleteInvalids(array) {
+    if (Array.isArray(array) === true) {
+        const outputArray = [];
+        for (i = 0; i < array.length; i++) {
+            if (!isNaN(array[i]) && typeof array[i] === 'number') {
+                outputArray.push(array[i]);
             }
-            return outputArray;
-         }
-         else {
-             return "Invalid Array";
-         }
+
         }
+        return outputArray;
+    }
+    else {
+        return "Invalid Array";
+    }
+}
 
-        ///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
 
-        
+function password(obj) {
+    let i = 0;
+    for (const item in obj2) {
+        i++;
+    }
+    let siteName1=obj.siteName[0].toUpperCase();
+    let siteNameRest= obj.siteName.split("");
+    siteNameRest.shift();
+    siteNameRest.unshift(siteName1);
+    let siteFinal=siteNameRest.join("");
+
+    let year=obj.birthYear;
+    let yearStr=year.toString();
+    let password =siteFinal+"#"+obj.name+"@"+yearStr;
+
+    if (i<3 || yearStr.length!==4) {
+        return "Invalid";
+    }
+    else {
+        return password;        
+    }
+}
+///////////////////////////
+function monthlySavings(arr, livingCost) {
+    if (Array.isArray(arr) === true && !isNaN(livingCost) && typeof livingCost === 'number') {
+        let totalSavings = 0, tax = 0, totalIncome = 0;
+        for (const element of arr) {
+            totalIncome = totalIncome + element;
+            if (element >= 3000) {
+                tax = element * .2;
+            }
+        }
+        totalSavings = totalIncome - tax - livingCost;
+        if (totalSavings >= 0) {
+            return totalSavings;
+        }
+        else {
+            return 'earn More';
+        }
+    }
+    else {
+        return "invalid input";
+    }
+}
 
 
-        let bb=['1' , {num:2} , NaN ] ;
-        let bbb=[ 1 , 2 , -3 ] ;
-        let bbbb=[1 , null, undefined, 18, -19, NaN, "12", [1, 2], { ob: "lala" }];
-        let bbbbb={num: [ 1 , 2 , 3 ]} ;
-console.log(deleteInvalids(a));
+const array1 = [21321423, 143423, 512, 51235, 5145, 646, 6347];
+
+const obj2 = {
+    name: 'kolimuddin',
+    birthYear: 1999,
+    siteName: 'google'
+
+}
+
+console.log(password(obj2));
+
+// let bb = ['1', { num: 2 }, NaN];
+// let bbb = [1, 2, -3];
+// let bbbb = [1, null, undefined, 18, -19, NaN, "12", [1, 2], { ob: "lala" }];
+// let bbbbb = { num: [1, 2, 3] };
+// console.log(deleteInvalids(a));
 // deleteInvalids(a);
+
+// const objj = {
+//     anfa: "afdaf",
+//     afdfa: "ewafdf",
+//     asdfa: 112
+// }
+
+
+// console.log(i);
+// const name2='name';
+// const firstLetterOfSiteName=obj2.siteName[0];
+// obj2.siteName.replace(siteName[0], G)
+// console.log(firstLetterOfSiteName);
+// console.log(obj2.siteName);
+// console.log(obj2[name2]);
+// console.log(Object.values(objj));
+// console.log(Object.values(objj)[0][0]);
